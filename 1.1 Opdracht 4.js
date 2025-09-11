a0ea22a983bb0e57c583b8b1a1c199a1f6a599c1
Lukas-0101 <111737@st-maartenscollege.nl>
2025-09-11 12:55:53 +0000

    les

---
+++ 1.1 Opdracht 4.js
@@ -0,0 +1,40 @@
+function setup() {
+  canvas = createCanvas(450,450);
+  background('cornflowerblue');
+  canvas.parent();
+  noLoop();
+}
+
+function draw() {
+  // grond
+  noStroke();
+  fill('wheat');
+  rect(0,350,450,100);
+  // huis
+  strokeWeight(4);
+  stroke('darkgrey');
+  fill('lightgray');
+  rect(100,280,100,100);
+  fill('gray');
+  // dak
+  strokeWeight(4);
+  stroke('darkgrey');
+  fill('grey');
+  triangle(100,280,200,280,150,200);
+  fill('gray');
+  // teken de deur
+  noStroke();
+  rect(125,330,30,50);
+  // teken de maan
+  fill('khaki');
+  ellipse(350,100,150)
+  
+  // boom
+  fill('sienna');
+  rect(300,250,40,130);
+  fill('olive');
+  ellipse(320,200,110,170)
+  // VERVANG DEZE REGEL door een regel die het groen van de boom tekent met behulp van ellipse
+  // Als je een cirkel wilt tekenen met diameter 300 kun je ellipse(0,0,300) gebruiken.
+  // Als je een ellipse wilt maken met breedte 300 en hoogte 500, dan moet je een parameter toevoegen: ellipse(0,0,300,500).
+}
